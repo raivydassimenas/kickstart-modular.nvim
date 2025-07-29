5c03400d407bef63b7cbc4b992135ceecdbb81fe
Raivydas Šimėnas <raivydas.simenas@proton.me>
2025-07-29 15:26:19 +0300

    Update lsp.lua
    Change tsserver to ts_ls in ensure installed

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -2,7 +2,7 @@ return {
   {
     'mason-org/mason-lspconfig.nvim',
     opts = {
-      ensure_installed = { 'lua_ls', 'jdtls', 'pyright', 'rust_analyzer', 'tsserver' }, -- list any language servers you want auto-installed
+      ensure_installed = { 'lua_ls', 'jdtls', 'pyright', 'rust_analyzer', 'ts_ls' }, -- list any language servers you want auto-installed
     },
     dependencies = {
       { 'mason-org/mason.nvim', opts = {} },
